ae7f88a9597979b24c8a5db576c2e3f0e84ac591
MegarjChauhan <megrajchauhan123@gmail.com>
2024-05-12 21:04:25 +0545

    I hate it

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 // Testing the preprocessor
 
-#include <iostream>
-#include "includes/preprocessor.hpp"
+// #include <iostream>
+// #include "includes/preprocessor.hpp"
 
-int main()
-{
-    using namespace zeta;
-    prep::Preprocessor p("test.zasm");
-    p.process();
-}
+// int main()
+// {
+//     using namespace zeta;
+//     prep::Preprocessor p("test.zasm");
+//     p.process();
+//     for (auto x : p.get_result())
+//     {
+//         std::cout << x;
+//     }
+// }
